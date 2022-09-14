419f2174c72c277ddac5c365b94728204eb420b1
Prashant95861 <pipaliyaprashant6002@gmail.com>
2022-09-14 14:27:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <div className="card w-25">
+        <div>
+          <h1 className="text-center">Hello</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
